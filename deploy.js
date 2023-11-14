9f41e13536092986350ef04eca8fdc543e5bbac9
Ujayata <62179207+Ujayata@users.noreply.github.com>
2023-11-14 20:39:22 +0100

    Update deploy.js
    In the deploy.js script, the private key is accessed using process.env.PRIVATE_KEY. While this is a common practice, ensure that the private key is properly protected and not hardcoded in the script. Consider using a tool like dotenv to load environment variables from a .env file securely.

--- deploy.js
+++ deploy.js
@@ -1,28 +1,33 @@
 // deploy.js
-const contract = require('./Voting.json'); // Make sure to compile your smart contract first
+require('dotenv').config(); // Load environment variables from .env file
+const contract = require('./Voting.json');
 const kit = require('@celo/contractkit');
 
 // Function to deploy the smart contract
 async function deployContract() {
-    // Initialize Celo ContractKit
-    const kitInstance = await kit.newKit('https://alfajores-forno.celo-testnet.org');
-    kitInstance.addAccount(process.env.PRIVATE_KEY);
+    try {
+        // Initialize Celo ContractKit
+        const kitInstance = kit.newKit(process.env.CELO_NETWORK_URL);
+        kitInstance.addAccount(process.env.PRIVATE_KEY);
 
-    // Get the deployer's account
-    const accounts = await kitInstance.web3.eth.getAccounts();
-    const deployer = accounts[0];
+        // Get the deployer's account
+        const accounts = await kitInstance.web3.eth.getAccounts();
+        const deployer = accounts[0];
 
-    // Create a new instance of the smart contract
-    const instance = new kitInstance.web3.eth.Contract(contract.abi);
-    const deploy = instance.deploy({ data: contract.bytecode });
+        // Create a new instance of the smart contract
+        const instance = new kitInstance.web3.eth.Contract(contract.abi);
+        const deploy = instance.deploy({ data: contract.bytecode });
 
-    // Deploy the contract to the Celo blockchain
-    const newContract = await deploy.send({
-        from: deployer,
-        gas: 5000000,
-    });
+        // Deploy the contract to the Celo blockchain
+        const newContract = await deploy.send({
+            from: deployer,
+            gas: 5000000,
+        });
 
-    console.log('Contract deployed at:', newContract.options.address);
+        console.log('Contract deployed at:', newContract.options.address);
+    } catch (error) {
+        console.error('Error deploying contract:', error);
+    }
 }
 
 deployContract();
